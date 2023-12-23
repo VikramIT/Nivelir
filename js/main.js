b1ba27c1e5cebb9efff58727014e4ee7d1ce2a33
Valera <epoxy-masters@mail.ru>
2023-12-23 22:35:22 +0300

    Это наш первый комиит

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// function checkTheGuck(){
+
+// }
+// window.onload = function(){
+//     checkTheGuck();
+
+// }
+
+
+
+
+
+
+let a = ["1","2","3","5"]; 
+function vievra(arr){
+    for(i=0;i<length.arr;i++){
+        console.log([i])
+    }
+}
+
+
+vievra(a);
