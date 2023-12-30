ca2067dfd737ef2abfa741098fc9598d7be0f9ff
Onix <epoxy-masters@mail.ru>
2023-12-30 16:37:10 +0300

    Первый комит О

--- js/main.js
+++ js/main.js
@@ -1,22 +1,187 @@
-// function checkTheGuck(){
-
-// }
-// window.onload = function(){
-//     checkTheGuck();
-
-// }
+function checkTheGuck(){
 
 
+let money = 100000;
+let time ="2023-12-30";
+
+
+let appData = {
+	budget: money,
+	timeData: time,
+    expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: false,
+    'EMAIL-sheet':"Dick-pik"
+
+ };
+    
+let value;
+
+value = appData.budget;
 
 
 
 
-let a = ["1","2","3","5"]; 
-function vievra(arr){
-    for(i=0;i<length.arr;i++){
-        console.log([i])
-    }
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется?");
+
+appData.expenses[a1]=a2;
+appData.expenses[a3]=a4;
+
+let freeBuget = (Number(appData.expenses[a1]))+(Number(appData.expenses[a3]));
+let freeBuget2 = ((+appData.expenses[a1])+ (+appData.expenses[a3]));
+alert(Math.floor(appData.budget-freeBuget2)/30+" Ваш ежедневный возможный бюджет");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+window.onload = function(){
+    checkTheGuck();
+
 }
 
 
-vievra(a);
